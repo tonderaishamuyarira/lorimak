37000a7f71fbefd2b84188e2b85e8c9d94c8ad4b
tonderaishamuyarira <38455487+tonderaishamuyarira@users.noreply.github.com>
2018-08-09 11:01:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,9 +25,9 @@ $password = "8a300288";
 $db = "heroku_86cdb196b426113";
 $connection = new mysqli($server, $username, $password, $db);
 
-$sql2 = "insert into pending_payslips(project_type,professionalism,know_how,timely_completion,value_for_money,recommend,satisfaction,recommendations) VALUES ($project_type,$professionalism,$know_how,$timely_completion,$value_for_money,$recommend,$satisfaction,$recommendations)";
+$sql2 = "insert into feedback(project_type,professionalism,know_how,timely_completion,value_for_money,recommend,satisfaction,recommendations) VALUES ($project_type,$professionalism,$know_how,$timely_completion,$value_for_money,$recommend,$satisfaction,$recommendations)";
 $result2 = $connection->query($sql2);
-$speech = "Thank you for your valued feedback. If you have any additional feedback or questions we'd love to hear from you. You can email us on info@lorimak.co.zw.";
+$speech = "Thank you for your valued feedback. This will help us in improving the services we offer. If you have any additional feedback or questions we'd love to hear from you. You can email us on info@lorimak.co.zw.";
 
 
 	$response = new \stdClass();
